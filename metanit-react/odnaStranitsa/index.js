33651e7aba9c27e3e2d1443ab1ed067bf03d381e
Anton Sharlai <sharlaychik@gmail.com>
2021-12-05 23:30:43 +0200

    1

--- metanit-react/odnaStranitsa/index.js
+++ metanit-react/odnaStranitsa/index.js
@@ -2,7 +2,6 @@ class Hello extends React.Component {
   render() {
     return /*#__PURE__*/React.createElement("h1", null, "Hello, React");
   }
-
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(Hello, null), document.getElementById("app"));
