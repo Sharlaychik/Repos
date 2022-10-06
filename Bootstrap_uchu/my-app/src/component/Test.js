c0af039e7a34099474226320036d6e6b77c105bb
Anton Sharlai <sharlaychik@gmail.com>
2022-10-06 11:34:39 +0300

    hgh

--- Bootstrap_uchu/my-app/src/component/Test.js
+++ Bootstrap_uchu/my-app/src/component/Test.js
@@ -2,45 +2,15 @@
 function Test() {
     return (
 
-        <div className="conteiner">
-          <h1>Zagolovok djfslkflksdf sdklfj;sdklfj;slkfj ;slkdfj;skldjf;skldfj;l ;slkdfjs;kldfj;asklfj ;slkdjf;aklsfj;askldfj ;lskdjf;asklfjs;ldkfj ;aslkdfj;skldfjs;dlkfj ;skldfjs;kldfjs;dklfj</h1>
-          <p>podzagolovok.</p>
+        // перебор через мап.
+        const names = ["whale", "squid", "turtle", "coral", "starfish"];
 
-          <div className="row">
-              <div className="col-md-6"> sldkjf sdjfaf nskf jslj kfnslj kfnls jkdfn alsj kdnfl sjkdfn aslk jdfn lasjkfnlsjkdnflskjdfn alskjfnlsdjkfnskjdfn kjnkn
-              </div>
-              <div className="col-md-6">kjnkn
-              </div>
-          </div>
+        const NamesList = () => (
+        <div>
+            <ul>{names.map(name => <li key={name}> {name} </li>)}</ul>
+        </div>
+        );
 
-          <div className="row">
-              <div className="col-md-2">kjnkn
-              </div>
-              <div className="col-md-2">kjnkn
-              </div>
-              <div className="col-md-2">kjnkn
-              </div>
-              <div className="col-md-2">kjnkn
-              </div>
-              <div className="col-md-2">kjnkn
-              </div>
-              <div className="col-md-2">kjnkn
-              </div>
-          </div>
-
-          <div className="row">
-              <div className="col-md-3">kjnkn
-              </div>
-              <div className="col-md-3">kjnkn
-              </div>
-              <div className="col-md-2">kjnkn
-              </div>
-              <div className="col-md-2">kjnkn
-              </div>
-              <div className="col-md-2">kjnkn
-              </div>
-          </div>
-    </div>
     
     );
 }
