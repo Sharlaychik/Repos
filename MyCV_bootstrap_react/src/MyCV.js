0015e31d21cf4a86f23f01bdb95d0789f9dda966
Anton Sharlai <sharlaychik@gmail.com>
2022-10-20 20:11:57 +0300

    add: carousel-center

--- MyCV_bootstrap_react/src/MyCV.js
+++ MyCV_bootstrap_react/src/MyCV.js
@@ -8,18 +8,20 @@ function MyCV() {
     <div id="MyCV" className='row'>
         
         <div id='MycvLeftCol' className='col-md-4 bg-secondary'>
-          <div id='MyPhoto' className='row'>
-            <div className='col-md-12'>
-            <PhotoCarousel />
-            </div>
+          <div id='MyPhoto' className='row justify-content-center '>
+                <PhotoCarousel />
           </div>
           <div id='MyInfo' className='row'>
-            <p className='text-center text-white fs-1 fw-bold'>Anton Sharlai</p>
+            <p className='text-center text-white fs-1 fw-bold'>
+              Anton Sharlai
+              </p>
           </div>
           <div>
             <hr className='border border-2'></hr>
             <p className='text-start text-white fs-5 fw-bold lh-sm'><img width='40rem' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Location_icon_from_Noun_Project.png/768px-Location_icon_from_Noun_Project.png' alt="position"></img>Contacts:</p>
-            <p className='text-center text-white fs-5 fw-bold'>Ukraine, Kiev</p>
+            <p className='text-center text-white fs-5 fw-bold'>
+              Ukraine, Kiev
+              </p>
             <hr className='border border-2'></hr>
            </div>
         </div>
