7d8523e6fe074f53c3151d28ce5acd164c9a0636
Anton Sharlai <sharlaychik@gmail.com>
2023-11-22 16:09:17 +0200

    add: sync posle dolgoy pauzi

--- MyCV_bootstrap_react/src/components/Testing.jsx
+++ MyCV_bootstrap_react/src/components/Testing.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 
 
+
 function Testing() {
   const [counter, setCounter] = useState(0);
 
@@ -19,5 +20,11 @@ function Testing() {
     <button onClick={increment}>increment</button>
   </div>
 }
+
+TRN_215/75_R17,5_M788_ALL_BS
+TRN_235/75_R17,5_M729_D_BS
+TRN_265/70_R17,5_M729_D_BS
+TRN_285/70_R19,5_M729_D_BS
+
 
 export default Testing;
